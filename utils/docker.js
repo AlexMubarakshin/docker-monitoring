9f34887c9215ccb5cb2042dd29143933ac3f8361
alm2 <Alm2>
2018-12-28 14:57:26 +0300

    Update parsing

--- utils/docker.js
+++ utils/docker.js
@@ -4,12 +4,12 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = {
     getProcess: async () => {
         try {
-            const stdOut = await exec("docker ps --all --format '{{.ID}},{{.Names}},{{.Ports}},{{.CreatedAt}}'");
+            const stdOut = await exec("docker ps --all --format '{{.ID}}*{{.Names}}*{{.Ports}}*{{.CreatedAt}}'");
             if (stdOut.stdout) {
                 const list = stdOut.stdout.replace(/'/g, "").split('\n');
 
                 const parsedList = list.map(element => {
-                    const processArr = element.split(',');
+                    const processArr = element.split('*');
                     return {
                         id: processArr[0],
                         name: processArr[1],
@@ -24,6 +24,7 @@ module.exports = {
             return [];
 
         } catch (err) {
+            console.log(err);
             return [];
         }
     }
