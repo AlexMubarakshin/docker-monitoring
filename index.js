392ac0c2b027a7b64e663a396ab895399b3e78ba
alm2 <Alm2>
2018-12-28 14:52:03 +0300

    Minor update

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const Table = require('cli-table');
+const commands = require('./utils/commands');
 
-const table = new Table({
-    head: ["ID", "NAME", "PORTS", "CREATED_AT"], 
-    colWidths: [50, 50, 25, 35]
-});
-
-const { getProcess } = require('./utils/docker');
-
-(async function () {
-    const processes = await getProcess();
-
-    let list = "";
-    // processes.forEach((process) => {
-    //     list += `ID: ${process.id} | NAME: ${process.name} | PORTS ${process.ports} | CREATED_AT ${process.createdAt}\n`
-    // })
-
-    // const response = `Total process: ${processes.length}\n${list}`;
-
-    processes.forEach(element => {
-        table.push([element.id, element.name, element.ports, element.createdAt]);
-        
-    });
-
-
-    console.log(table.toString());
-})();
+(async () => {
+    console.log(await commands.getProcess());
+})()
 
---
+++ utils/commands.js
@@ -0,0 +1,25 @@
+const Table = require('cli-table');
+
+const table = new Table({
+    head: ["ID", "NAME", "PORTS", "CREATED_AT"], 
+    colWidths: [50, 50, 25, 35]
+});
+
+const { getProcess } = require('./docker');
+
+module.exports = {
+    getProcess: async () => {
+
+        const processes = await getProcess();
+        
+        processes.forEach(element => {
+            table.push([element.id, element.name, element.ports, element.createdAt]);
+            
+        });
+        
+        
+        return table.toString();
+    }
+}
+
+
