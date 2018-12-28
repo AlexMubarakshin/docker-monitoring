35f9b0b243bde6aa803ebcfab01eec1277af575d
alm2 <Alm2>
2018-12-28 14:22:02 +0300

    Add tables

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+const Table = require('cli-table');
+
+const table = new Table({
+    head: ["ID", "NAME", "PORTS", "CREATED_AT"], 
+    colWidths: [50, 50, 25, 35]
+});
+
 const { getProcess } = require('./utils/docker');
 
 (async function () {
     const processes = await getProcess();
 
     let list = "";
-    processes.forEach((process) => {
-        list += `ID: ${process.id} | NAME: ${process.name} | PORTS ${process.ports} | CREATED_AT ${process.createdAt}\n`
-    })
+    // processes.forEach((process) => {
+    //     list += `ID: ${process.id} | NAME: ${process.name} | PORTS ${process.ports} | CREATED_AT ${process.createdAt}\n`
+    // })
 
-    const response = `Total process: ${processes.length}\n${list}`;
+    // const response = `Total process: ${processes.length}\n${list}`;
 
-    console.log(response);
+    processes.forEach(element => {
+        table.push([element.id, element.name, element.ports, element.createdAt]);
+        
+    });
+
+
+    console.log(table.toString());
 })();
 
